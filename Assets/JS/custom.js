7b844987f07c215ee2ec23718353fce7fb2759f6
fire8327 <114702513+fire8327@users.noreply.github.com>
2023-06-16 02:26:05 +0300

    create swiper

--- Assets/JS/custom.js
+++ Assets/JS/custom.js
@@ -7,3 +7,36 @@ $("#toggler, #overlay").click(()=>{
   $("body").toggleClass("overflow-hidden")
   $("#overlay").toggleClass("hidden")
 })
+/* swiper */
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  pagination: {
+    el: '.offers-pagination',
+  },
+  navigation: {
+    nextEl: '.offers-next',
+    prevEl: '.offers-prev',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      spaceBetween: 0
+    },
+    768: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 30
+    },
+    1024: {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+      spaceBetween: 50
+    },
+    1440: {
+      slidesPerView: 5,
+      slidesPerGroup: 5,
+      spaceBetween: 50
+    }
+  }
+});
